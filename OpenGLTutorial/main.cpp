88a2aea3fa4a52b528d1e3b5485a777f14ecdbbe
luw5008 <37829455+LukasW352435@users.noreply.github.com>
2021-02-24 21:17:53 +0100

    Add project files.

---
+++ OpenGLTutorial/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
+#pragma comment(lib, "SDL2.lib")
+#pragma comment(lib, "glew32s.lib")
+#pragma comment(lib, "opengl32.lib")
+
+#pragma region int main(int argc, char** argv)
+#ifdef _DEBUG
+int main(int argc, char** argv) {
+#else
+int WinMain(int argc, char** argv) {
+#endif // _DEBUG
+#pragma endregion
+	SDL_Window* window;
+	SDL_Init(SDL_INIT_EVERYTHING);
+	window = SDL_CreateWindow("OpenGL Tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+	SDL_GLContext glContext = SDL_GL_CreateContext(window);
+
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	bool close = false;
+	while (!close)
+	{
+		glClearColor(1, 0, 0, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
+		SDL_GL_SwapWindow(window);
+
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT) {
+				close = true;
+			}
+		}
+	}
+	return 0;
+}
